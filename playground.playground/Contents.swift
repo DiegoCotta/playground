4b0a009285c83b4522dbe1e69ad6ca7b9c44cf95
Diego Cotta <dcotta15@gmail.com>
2023-05-15 20:42:02 -0300

    desafio dio playground

---
+++ playground.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+let constante = "Steve"
+var variavel: String? = "Jobs"
+var resultado: String?
+if let variavel {
+    print(constante, variavel, separator: " ")
+} else {
+    print(constante, "Wozniak", separator: " ")
+}
